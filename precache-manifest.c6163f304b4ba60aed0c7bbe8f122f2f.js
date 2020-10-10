d7247f827fe5c8f67e42eaf5c53f31d9129f2400
Admin <D:\[4EK.net]\XLAM\mail>
2020-10-10 18:26:14 +0300

    Updates

---
+++ precache-manifest.c6163f304b4ba60aed0c7bbe8f122f2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81132ccf2edc655871097df6d4ace539",
+    "url": "/index.html"
+  },
+  {
+    "revision": "769d3350d2c57a3f722e",
+    "url": "/static/css/main.8c7131ac.chunk.css"
+  },
+  {
+    "revision": "5024aea1eee7b8ae6316",
+    "url": "/static/js/2.fc588c11.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.fc588c11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "769d3350d2c57a3f722e",
+    "url": "/static/js/main.7cdc84e8.chunk.js"
+  },
+  {
+    "revision": "6f687d57c08bc24554a9",
+    "url": "/static/js/runtime-main.b8f0b7fd.js"
+  }
+]);
